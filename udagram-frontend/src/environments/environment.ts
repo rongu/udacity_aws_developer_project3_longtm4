8933d58b35dc4b408ae52e037f2a0bfc333128ac
Long <longtm.fotech@gmail.com>
2023-10-27 00:33:22 +0900

    Update frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a22852b81148342fca115ee72019b19e-1408635372.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
